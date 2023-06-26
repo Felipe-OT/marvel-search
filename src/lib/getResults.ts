c55721880553637c19729214fd10517015be25ae
Felipe-OT <felipe.rm@outlook.com.br>
2023-06-26 15:02:31 -0300

    hero type getRsults

--- src/lib/getResults.ts
+++ src/lib/getResults.ts
@@ -1,14 +1,21 @@
 import { NextResponse } from "next/server";
 
+type HeroType = {
+  thumbnail: { path: string | string[] };
+  comics: { available: number };
+  events: { available: number };
+  series: { available: number };
+};
+
 export default async function getResults(searchParams: string) {
-  console.log('chamou')
+  console.log("chamou");
   const res = await fetch(
     `https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${searchParams}&apikey=d158498c097567406c1e29f6202a5f4f&hash=43a2930ceed84cfe63db8b5efe33599c&ts=1`
   );
 
   const data = await res.json();
 
-  const removeImgNotFound = data.data.results.filter((item) => {
+  const removeImgNotFound = data.data.results.filter((item: HeroType) => {
     if (item.thumbnail.path.indexOf("image_not_available") > -1) {
       return false;
     } else if (
